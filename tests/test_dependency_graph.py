1b7b8bbc2b207ef64aab585054df67f351828ba8
Henrik Finsberg <henriknf@simula.no>
2022-04-04 21:15:23 +0200

    100% coverage

--- tests/test_dependency_graph.py
+++ tests/test_dependency_graph.py
@@ -4,6 +4,7 @@ import gotran
 import pytest
 from modelgraph import DependencyGraph
 
+
 here = Path(__file__).absolute().parent
 ode_file = here.parent.joinpath("demo").joinpath(
     "hodgkin_huxley_squid_axon_model_1952_original.ode",
@@ -74,3 +75,89 @@ def test_direct_inv_dependents(graph, name, expected_dependents):
 def test_indirect_inv_dependents(graph, name, expected_dependents):
     dependents = graph.inv_dependents(name, direct=False)
     assert dependents == expected_dependents
+
+
+def test_repr(graph):
+    assert (
+        repr(graph) == "DependencyGraph(hodgkin_huxley_squid_axon_model_1952_original)"
+    )
+
+
+@pytest.mark.parametrize(
+    "name, expected_edges",
+    [
+        (
+            "i_K",
+            set(
+                (
+                    ("i_K", "V"),
+                    ("i_K", "g_K"),
+                    ("i_K", "E_K"),
+                    ("i_K", "n"),
+                    ("E_K", "E_R"),
+                ),
+            ),
+        ),
+        ("i_L", set((("i_L", "V"), ("i_L", "g_L"), ("i_L", "E_L"), ("E_L", "E_R")))),
+        (
+            "dm_dt",
+            set(
+                (
+                    ("dm_dt", "m"),
+                    ("dm_dt", "beta_m"),
+                    ("dm_dt", "alpha_m"),
+                    ("beta_m", "V"),
+                    ("alpha_m", "V"),
+                ),
+            ),
+        ),
+        ("g_L", set()),
+    ],
+)
+def test_depedency_graph(graph, name, expected_edges):
+    G = graph.dependency_graph(name)
+    assert set(G.edges) == expected_edges
+
+
+@pytest.mark.parametrize(
+    "name, expected_edges",
+    [
+        ("g_Na", set((("g_Na", "i_Na"), ("i_Na", "dV_dt")))),
+        (
+            "E_R",
+            set(
+                (
+                    ("E_R", "E_Na"),
+                    ("E_R", "E_K"),
+                    ("E_R", "E_L"),
+                    ("E_Na", "i_Na"),
+                    ("i_Na", "dV_dt"),
+                    ("E_K", "i_K"),
+                    ("i_K", "dV_dt"),
+                    ("E_L", "i_L"),
+                    ("i_L", "dV_dt"),
+                ),
+            ),
+        ),
+        ("dV_dt", set()),
+        (
+            "Cm",
+            set((("Cm", "dV_dt"),)),
+        ),
+    ],
+)
+def test_inv_depedency_graph(graph, name, expected_edges):
+    G = graph.inv_dependency_graph(name)
+    assert set(G.edges) == expected_edges
+
+
+def test_depdedent_names(graph, ode):
+    assert set(graph.dependent_names) == set(
+        [v.name for v in ode.intermediates + ode.state_expressions],
+    )
+
+
+def test_inv_depdedent_names(graph, ode):
+    assert set(graph.inv_dependent_names) == set(
+        [v.name for v in ode.intermediates + ode.parameters + ode.states] + ["t"],
+    )
