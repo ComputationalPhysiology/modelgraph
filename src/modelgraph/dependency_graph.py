c8785957e8c7e8540a36c6eb37bf64d663a4eefc
Henrik Finsberg <henriknf@simula.no>
2022-04-05 09:49:10 +0200

    Add some docstrings

--- src/modelgraph/dependency_graph.py
+++ src/modelgraph/dependency_graph.py
@@ -37,6 +37,13 @@ def _build_graph(func: Callable[[str], set[str]], arg: str) -> nx.DiGraph:
 
 class DependencyGraph:
     def __init__(self, ode: gotran.ODE) -> None:
+        """Intialize dependency graph
+
+        Parameters
+        ----------
+        ode : gotran.ODE
+            The model represented as a `gotran.ODE`
+        """
 
         self._dependents: dict[str, set[str]] = {}
         self._inv_dependents: dict[str, set[str]] = defaultdict(set)
@@ -44,7 +51,7 @@ class DependencyGraph:
         self._load_dependents()
 
     def _load_dependents(self):
-
+        """Load dependencies from ode"""
         for intermediate in self._ode.intermediates + self._ode.state_expressions:
 
             self._dependents[intermediate.name] = set()
@@ -64,17 +71,47 @@ class DependencyGraph:
 
     @property
     def dependent_names(self) -> list[str]:
+        """Return all the names of the expressions having dependencies
+
+        Returns
+        -------
+        list[str]
+            List of names
+        """
         return list(self._dependents.keys())
 
     @property
     def inv_dependent_names(self) -> list[str]:
+        """Return all the names of the expressions that other expressions depdens on
+
+        Returns
+        -------
+        list[str]
+            List of names
+        """
         return list(self._inv_dependents.keys())
 
     @property
     def ode(self) -> gotran.ODE:
+        """Return the ode"""
         return self._ode
 
     def dependents(self, name: str, direct: bool = True) -> set[str]:
+        """Given a name of an expression return its dependencies
+
+        Parameters
+        ----------
+        name : str
+            Name of the expression
+        direct : bool, optional
+            Whether to return only direct depenencies or also include
+            indirect dependencies, by default True
+
+        Returns
+        -------
+        set[str]
+            Set of names of all dependencies
+        """
         if direct:
             return self._dependents.get(name) or set()
 
@@ -82,6 +119,22 @@ class DependencyGraph:
         return _add_dependents_recursively(self.dependents, name)
 
     def inv_dependents(self, name: str, direct: bool = True) -> set[str]:
+        """Given a name of an expression return all expressions
+        that depends on it
+
+        Parameters
+        ----------
+        name : str
+            Name of the expression
+        direct : bool, optional
+            Whether to return only direct depenencies or also include
+            indirect dependencies, by default True
+
+        Returns
+        -------
+        set[str]
+            Set of names of all dependencies
+        """
         if direct:
             return self._inv_dependents.get(name) or set()
 
@@ -89,7 +142,42 @@ class DependencyGraph:
         return _add_dependents_recursively(self.inv_dependents, name)
 
     def dependency_graph(self, name: str) -> nx.DiGraph:
+        """Get the dependency graph where each node reperesents
+        an expression and a directed edge means that there is
+        a dependency.
+
+        There will be a directed edge from the root node to
+        another node if the root node is a dependency of the
+        other node.
+
+        Parameters
+        ----------
+        name : str
+            Name of the root node
+
+        Returns
+        -------
+        nx.DiGraph
+            A directed dependency graph
+        """
         return _build_graph(self.dependents, name)
 
     def inv_dependency_graph(self, name: str):
+        """Get the dependency graph where each node reperesents
+        an expression and a directed edge means that there is
+        a dependency.
+
+        There will be a directed edge from the root node to
+        another node if the root node depends on the other node.
+
+        Parameters
+        ----------
+        name : str
+            Name of the root node
+
+        Returns
+        -------
+        nx.DiGraph
+            A directed dependency graph
+        """
         return _build_graph(self.inv_dependents, name)
